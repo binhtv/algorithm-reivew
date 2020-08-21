85137c4fd94f1049c6d90be216168b5b93970bef
Binh Tran <binh12121989@gmail.com>
2020-08-21 07:38:02 -0500

    Build Tree from the result of traversal

---
+++ src/main/java/tree/BuildTreePostOrder.java
@@ -0,0 +1,58 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BuildTreePostOrder {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private int idx = 0;
+    private Map<Integer, Integer> lookup = new HashMap<>();
+
+    public BuildTreePostOrder(int end) {
+        idx = end;
+    }
+
+    private TreeNode build(int[] postorder, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(postorder[idx--]);
+
+        int rIndex = lookup.get(node.val);
+        node.right = build(postorder, rIndex + 1, end);
+        node.left = build(postorder, start, rIndex - 1);
+
+        return node;
+    }
+
+    public TreeNode buildFromPostOrder(int[] postorder, int[] inorder) {
+        if(postorder == null || inorder == null || postorder.length == 0 || postorder.length != inorder.length) {
+            return null;
+        }
+
+        for(int i = 0; i < inorder.length; i++) {
+            lookup.put(inorder[i], i);
+        }
+
+        return build(postorder, 0, postorder.length - 1);
+    }
+
+    public static void main(String... strings) {
+        int[] postorder = new int[]{9, 15, 7, 20, 3};
+        int[] inorder = {9, 3, 15, 20, 7};
+
+        BuildTreePostOrder buildTree = new BuildTreePostOrder(postorder.length - 1);
+        TreeNode root = buildTree.buildFromPostOrder(postorder, inorder);
+    }
+}
---
+++ src/main/java/tree/BuildTreePreOrder.java
@@ -0,0 +1,54 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BuildTreePreOrder {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    private int idx = 0;
+    private Map<Integer, Integer> lookup = new HashMap<>();
+
+    private TreeNode build(int[] preorder, int start, int end) {
+        if(start >= end) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(preorder[idx++]);
+
+        int rIndex = lookup.get(node.val);
+        node.left = build(preorder, start, rIndex);
+        node.right = build(preorder, rIndex + 1, end);
+
+        return node;
+    }
+
+    public TreeNode buildFromPreorder(int[] preorder, int[] inorder) {
+        if(preorder == null || inorder == null || preorder.length == 0 || preorder.length != inorder.length) {
+            return null;
+        }
+
+        for(int i = 0; i < inorder.length; i++) {
+            lookup.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, preorder.length);
+    }
+
+    public static void main(String... strings) {
+        int[] preorder = new int[]{3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+
+        BuildTreePreOrder buildTree = new BuildTreePreOrder();
+        TreeNode root = buildTree.buildFromPreorder(preorder, inorder);
+    }
+}
