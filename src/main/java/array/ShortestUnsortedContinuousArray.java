e03c730405585c827f60c34b4817933e8bc6611c
Binh Tran <binh12121989@gmail.com>
2020-10-01 08:42:38 -0500

    Shortest unsorted continuous array with stack

---
+++ src/main/java/array/ShortestUnsortedContinuousArray.java
@@ -0,0 +1,48 @@
+package array;
+
+import java.util.Stack;
+
+/**
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+ * <p>
+ * You need to find the shortest such subarray and output its length.
+ * <p>
+ * Example 1:
+ * Input: [2, 6, 4, 8, 10, 9, 15]
+ * Output: 5
+ * Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+ * Note:
+ * Then length of the input array is in range [1, 10,000].
+ * The input array may contain duplicates, so ascending order here means <=.
+ */
+public class ShortestUnsortedContinuousArray {
+    public static int findUnsortedSubarray(int[] nums) {
+        if (nums == null || nums.length == 1) {
+            return 0;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int l = nums.length, r = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] > nums[i]) {
+                l = Math.min(l, stack.pop());
+            }
+            stack.push(i);
+        }
+
+        stack.clear();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
+                r = Math.max(r, stack.pop());
+            }
+            stack.push(i);
+        }
+
+        return r > l ? r - l + 1 : 0;
+    }
+    
+    public static void main(String... strings) {
+        int[] nums1 = new int[]{2, 6, 4, 8, 10, 9, 15};
+        int[] nums = new int[]{2, 6, 4, 8, 10, 9, 5, 3, 7, 9, 1, 15};
+    }
+}
