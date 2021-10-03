41c4478fe239b13a7afe3f93b28bb1c6a5685385
Binh Tran <binh12121989@gmail.com>
2021-10-03 16:33:13 -0600

    MoveZeros and TwoSum + Utils

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Utils {
+    public static final void printArray(int[] arr) {
+        for(int i : arr) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
